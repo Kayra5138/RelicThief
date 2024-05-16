7cdf310bc6b970102eef80c77f4d2aa2e25821e8
Kayra <nihatkayra@gmail.com>
2024-05-16 21:40:16 +0300

    firikşın

--- movable_block.gd
+++ movable_block.gd
@@ -1,25 +1,24 @@
 extends CharacterBody2D
-@export var speed = 5000
+@export var speed = 100
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@export var friction = 400
+@export var friction = 500
 var pusher_groups = ["player","cultist"]
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity.x = move_toward(velocity.x,0,friction*delta)
 	for body in $Left.get_overlapping_bodies():
 		for group in pusher_groups:
 			if body.is_in_group(group):
-				velocity.x = speed * delta
+				velocity.x = speed
 	for body in $Right.get_overlapping_bodies():
 		for group in pusher_groups:
 			if body.is_in_group(group):
-				velocity.x = -speed * delta
+				velocity.x = -speed
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else :
 		velocity.y = 0
 	move_and_slide()
+
+func explode():
+	queue_free()
