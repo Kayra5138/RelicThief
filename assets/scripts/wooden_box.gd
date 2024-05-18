f2dda13e5ab3ea6f21b981e2c407380e97af2f4b
MecHR17 <52838067+MecHR17@users.noreply.github.com>
2024-05-18 17:53:08 +0300

    box fix

--- assets/scripts/wooden_box.gd
+++ assets/scripts/wooden_box.gd
@@ -1,6 +1,5 @@
 extends RigidBody2D
 
-
 const SPEED = 300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -11,3 +10,6 @@ var being_carried = false
 func _physics_process(delta):
 	if being_carried:
 		linear_velocity = Vector2.ZERO
+
+func explode():
+	queue_free()
