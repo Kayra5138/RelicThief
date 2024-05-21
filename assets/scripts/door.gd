32c29dadc9ced04175b2d93e944e4ece70d06725
MecHR17 <52838067+MecHR17@users.noreply.github.com>
2024-05-21 23:04:14 +0300

    Doors dont close when objects are inside

--- assets/scripts/door.gd
+++ assets/scripts/door.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+@onready var area: Area2D = $Area2D
+
 var tmp = []
 
 func open():
@@ -11,9 +13,12 @@ func close():
 	fln()
 
 func fln():
-	if len(tmp) == 0:
+	if len(tmp) == 0 and len(area.get_overlapping_bodies()) == 0:
 		$CollisionShape2D.set_deferred("disabled", false);
 		$AnimatedSprite2D.animation = "default";
 	else:
 		$CollisionShape2D.set_deferred("disabled", true);
 		$AnimatedSprite2D.animation = "open";
+
+func _on_area_2d_body_exited(body):
+	fln()
