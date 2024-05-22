14a53fe0c033cc5cda6b7d3286d7885e4538b92f
MirkanErdemli <mirkanerdemli@hotmail.com>
2024-05-22 19:59:41 +0300

    ui
    ui

---
+++ Levels.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+func _on__pressed():
+	pass # Replace with function body.
---
+++ levels_menu.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+
+func _on_level_2_pressed():
+	get_tree().change_scene_to_file("res://levels/level_2.tscn")
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://levels/level_3.tscn")
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file("res://levels/level_4.tscn")
+
+func _on_level_5_pressed():
+	get_tree().change_scene_to_file("res://levels/level_5.tscn")
+
+func _on_level_6_pressed():
+	get_tree().change_scene_to_file("res://levels_menu.tscn")
+
+func _on_level_7_pressed():
+	get_tree().change_scene_to_file("res://levels_menu.tscn")
+
+func _on_level_8_pressed():
+	get_tree().change_scene_to_file("res://levels_menu.tscn")
